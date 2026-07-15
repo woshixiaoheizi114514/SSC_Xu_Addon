81fbeae1ca63d1467913442a1369a086427330f3
xu233333 <18510478058@163.com>
2026-07-15 15:45:18 +0800

    更新SSC到最新版 又发现一个Bug

--- src/main/java/xu_mod/SSCXuAddon/init/Init_ManaType.java
+++ src/main/java/xu_mod/SSCXuAddon/init/Init_ManaType.java
@@ -367,17 +367,20 @@ public class Init_ManaType {
     );
 
     static {
-        // 被自己坑了 待会打包完加一个API
-        // ManaRegistries.getManaRegenModifier(ManaRegistries.FAMILIAR_FOX_MANA).add(
-        //         SSCXuAddon.identifier("creative"),
-        //         MC_IsCreative,
-        //         new ManaUtils.Modifier(0d, 1.0d, 1000d)  // +20000 per sec
-        // );
-        // ManaRegistries.getManaRegenModifier(ManaRegistries.WEB_RESOURCE).add(
-        //         SSCXuAddon.identifier("creative"),
-        //         MC_IsCreative,
-        //         new ManaUtils.Modifier(0d, 1.0d, 1000d)  // +20000 per sec
-        // );
+        ManaRegistries.modifyManaRegenModifier(ManaRegistries.FAMILIAR_FOX_MANA, modifierList -> {
+            modifierList.add(
+                    SSCXuAddon.identifier("creative"),
+                    MC_IsCreative,
+                    new ManaUtils.Modifier(0d, 1.0d, 1000d)  // +20000 per sec
+            );
+        });
+        ManaRegistries.modifyManaRegenModifier(ManaRegistries.WEB_RESOURCE, modifierList -> {
+            modifierList.add(
+                    SSCXuAddon.identifier("creative"),
+                    MC_IsCreative,
+                    new ManaUtils.Modifier(0d, 1.0d, 1000d)  // +20000 per sec
+            );
+        });
     }
 
 
